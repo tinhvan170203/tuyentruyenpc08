e8325dd540202ac3f14fd9d875252a9b86e8ea84
Vu Van Tinh <vuvantinh121123@gmail.com>
2025-12-20 20:37:49 +0700

    trien khai giao dien moi lan 2

--- dist/assets/axiosCommon-9012a0c8.js
+++
@@ -1,1 +0,0 @@
-import{aw as t,ax as n}from"./index-b04b101c.js";const r=t.create({baseURL:n,headers:{},withCredentials:!0});r.interceptors.request.use(function(e){return e},function(e){return Promise.reject(e)});r.interceptors.response.use(function(e){return e},function(e){return Promise.reject(e)});export{r as a};
