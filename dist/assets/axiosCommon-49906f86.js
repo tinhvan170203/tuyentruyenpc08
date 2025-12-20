441438ca6d20579da22fcf49be38e9a9c7ea282b
Vu Van Tinh <vuvantinh121123@gmail.com>
2025-12-20 20:30:37 +0700

    trien khai lai

--- dist/assets/axiosCommon-49906f86.js
+++
@@ -1,1 +0,0 @@
-import{aw as t,ax as n}from"./index-c0c1d070.js";const r=t.create({baseURL:n,headers:{},withCredentials:!0});r.interceptors.request.use(function(e){return e},function(e){return Promise.reject(e)});r.interceptors.response.use(function(e){return e},function(e){return Promise.reject(e)});export{r as a};
